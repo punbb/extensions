87babe4c1b7b61f2b8ed25a31a27f7133a00374a
denis <denis@4dc816b1-62af-4023-b237-cae3452c1eb1>
2008-12-08 15:31:03 +0000

    pun_quote: fixes
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@977 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_quote/scripts.js
+++ pun_quote/scripts.js
@@ -146,27 +146,13 @@ function Reply(tid_param, qid_param, d)
 		
 		for (i = 0; i < selNode.length; i++)
 		{
-<<<<<<< .mine
 			if (selNode[i].innerHTML.indexOf('Reply') != -1)
-=======
-			var post = new String(element[i].innerHTML);
-
-			if(post.search('Reply[(]' + tid_param + ',' + qid_param + '[)]') != -1)
->>>>>>> .r972
-			{
-<<<<<<< .mine
+			{	
 				if (d == selNode[i])
-=======
-				post=ChangePost(post);
-				var post_new = RemoveSymbols(post);
-				var selected_text = (window.selected_text_first == '')?window.selected_text_second:window.selected_text_first;//getSelectedText();
-				var reply_url = document.getElementById("pun_quote_url");
-				reply_url = reply_url.value;
-				var replace_url;
-
-				if((selected_text != undefined)&&(selected_text!=''))
->>>>>>> .r972
 				{
+					var reply_url = document.getElementById("pun_quote_url");
+					reply_url = reply_url.value;
+					var replace_url;
 					var pun_quote_post = d.parentNode.parentNode.parentNode.parentNode.parentNode.getElementsByTagName('div');
 					var flag = 1;
 					var i = 0;
@@ -190,11 +176,10 @@ function Reply(tid_param, qid_param, d)
 					replace_url = reply_url.replace('$2',qid_param.toString());
 					form.action = replace_url.toString();
 					form.submit();
+					break;
 				}
 				else
 					pun_quote_whole_citing = 1;
-					
-				break;
 			}
 		}
 	}
@@ -263,13 +248,8 @@ function QuickQuote(tid_param, qid_param, d)
 					
 					if((selected_text.type=='Range') || (selected_text.type=='Caret'))
 						selected_text=selected_text.toString();
-<<<<<<< .mine
-			
-=======
-
->>>>>>> .r972
+	
 					selected_text = RemoveSymbols(selected_text);
-			
 					element = document.getElementById('fld1');
 					element.value +=(window.selected_text_first == '')?'[quote='+author+']'+window.selected_text_second+'[/quote]'+'\n':'[quote='+author+']'+window.selected_text_first+'[/quote]'+'\n';					
 				}
@@ -322,96 +302,7 @@ function RemoveSymbols(string)
 	string = string.replace(/<BR>/ig,'');
 	return string;
 }
-<<<<<<< .mine
-=======
-function ChangePost(post)
-{
-	var reg = new RegExp('<DIV[\\s]*class[\\s]*=[\\s]*["]*[\\s]*entry\\-content[\\s]*["]*[\\s]*>[\\s\\S]*<DIV[\\s]*class[\\s]*=[\\s]*["]*[\\s]*postfoot[\\s]*["]*[\\s]*>','ig');
 
-	var post = new String(reg.exec(post));
-
-	var browse = navigator.userAgent.toLowerCase();
-
-	post = post.replace(/((<BR>)(<\/P>))|((<BR\/>)(<\/P>))/ig,'$2$4');
-
-	if(browse.indexOf('opera') == -1)
-		post = post.replace(/((<BR>)(<P>))|((<BR\/>)(<P>))/ig,'$2$4');
-
-	post = post.replace(/(:?<BR>)|(:?<BR\/>)/ig,'\n');
-
-	//</p><p> = \n\n  - Opera FF
-	//</p><p> = /n - IE 7.0
-	if(browse.indexOf('opera') != -1 ||  browse.indexOf('gecko') != -1)
-		post = post.replace(/(:?<\/p>)|(:?<p>)/ig,'\n');
-	else
-		post = post.replace(/<\/p>[\s]*<p>/ig,'\n');
-
-	post = post.replace(/>[\s]*</,'><');
-
-	//Make [quote="name"]...[/quote]
-	post = post.replace(/<div[\s]*class[\s]*=[\s]*["]*[\s]*quotebox[\s]*["]*[\s]*>[\s]*<cite>/ig,'[quote=');
-	post = post.replace(/<div[\s]*class[\s]*=[\s]*["]*[\s]*quotebox[\s]*["]*[\s]*>/ig,'[quote]');
-	post = post.replace(/[\s]*wrote:/g,"]");
-	post = post.replace(/[\s]*<\/blockquote>[\s]*/ig,'[/quote]\n');
->>>>>>> .r972
-
-<<<<<<< .mine
-=======
-	//Handle BB-codes
-	//code
-	post = post.replace(/<div[\s]*class[\s]*=[\s]*["]*[\s]*codebox[\s]*["]*[\s]*>[\s]*<pre>[\s]*<code>/ig,'[code]');
-	post = post.replace(/[\s]*<\/code>[\s]*<\/pre>[\s]*<\/div>/ig,'[/code]\n');	
-
-	//b
-	post = post.replace(/[\s]*<strong>[\s]*/ig,'[b]');
-	post = post.replace(/[\s]*<\/strong>[\s]*/ig,'[/b]');
-
-	//list
-	post = post.replace(/[\s]*<ul>[\s]*/ig,'[list]');
-	post = post.replace(/[\s]*<ol[\s]*class[\s]*=[\s]*["]decimal["][\s]*>[\s]*/ig,'[list=1]');
-	post = post.replace(/[\s]*<ol[\s]*class[\s]*=[\s]*["]alpha["][\s]*>[\s]*/ig,'[list=a]');
-	post = post.replace(/[\s]*<li>[\s]*/ig,'[*]');
-	post = post.replace(/[\s]*<\/li>[\s]*/ig,'[/*]');
-	post = post.replace(/[\s]*<\/ol>[\s]*/ig,'[/list]');
-	post = post.replace(/[\s]*<\/ul>[\s]*/ig,'[/list]');
-
-	for (temp = 0; temp < 6; temp++)
-	{
-		//color
-		post = post.replace(/<span[\s]*style[\s]*=[\s]*["]*color[\s]*:[\s]*([\#a-zA-Z0-9]*)\;{1}["]*[\s]*>([^\<]*)<\/span>[\s]*/ig,'[color=$1]$2[/color]');
-		//u
-		post = post.replace(/[\s]*<span[\s]*class[\s]*=[\s]*["]*bbu["]*[\s]*>([^\<]*)<\/span>[\s]*/ig,'[u]$1[/u]');	
-		//i
-		post = post.replace(/[\s]*<em>([^\<]*)<\/em>[\s]*/ig,'[i]$1[/i]');
-		//h
-		post = post.replace(/[\s]*<h5>([^\<]*)<\/h5>[\s]*/ig,'[i]$1[/i]');
-		//email
-		post = post.replace(/<a[\s]*href=["]*mailto:[\s]*([^]*)["]*[\s]*>([^]*)<\/a>/ig,'[email=\"$1\"]$2[/email]');
-		//url
-		post = post.replace(/<a[\s]*href=["]*http\:\/\/([^]*)["]*[\s]*>([^]*)<\/a>/ig,'[url=\"$1\"]$2[/url]');
-	}
-	
-	//Remove signature block
-	post = post.replace(/<div[\s]*class[\s]*=[\s]*["]*[\s]*sig-content[\s]*["]*[\s]*>(.*)<\/div>/gi,'');
-
-	//Remove tags
-	post = post.replace(/<(:?.*?)>/gi,'');
-
-	//Replace quote = name name on quote = "name name"
-	post = post.replace(/\[quote=(["][-a-zA-Z0-9]*)[\s]+([-"a-zA-Z0-9]*)\]/g,'[quote=\"$1 $2\"]');
-
-	//Insert \n before [/quote]
-	post = post.replace(/\]\[\/quote\]/g,']\n[/quote]');
-
-	//exotic symbols =)
-	post = post.replace(/\u00A0/g,' ');
-	post = post.replace(/&nbsp;/g,' ');
-	post = post.replace(/&lt;/g,'<');
-	post = post.replace(/&gt;/g,'>');
-	return post;
-}
-
->>>>>>> .r972
 function SetSelected()
 {
 	switch(window.selected_text_pointer)
