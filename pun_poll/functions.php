67da98bfdb8661db9da42205bdf8c6f5b96a81e8
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-01-22 16:00:32 +0000

    pun_poll: New keys in lang-file were added. Some format fixes. Code was optimize. Function for output of voting form was modified.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1011 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_poll/functions.php
+++ pun_poll/functions.php
@@ -2,134 +2,134 @@
 
 function form_of_poll($question, $poll_answers, $options_count, $days_poll, $votes_poll, $edit = false)
 {
-	global $forum_user, $lang_pun_poll, $forum_config, $forum_page;
+	global $forum_user, $lang_pun_poll, $forum_config, $forum_page, $read_unvote_users, $revote;
 
 ?>
 	<div class="ct-box info-box"><p><?php echo (!$edit ? $lang_pun_poll['New voting'] : $lang_pun_poll['Edit voting admin']); ?></p></div>
-    <fieldset class="frm-group group<?php echo ++$forum_page['group_count'] ?>">
-        <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
-            <div class="sf-box text">
-                <label for="fld<?php echo ++$forum_page['fld_count'] ?>">
-                    <span>
-                        <?php echo $lang_pun_poll['Poll question'] ?>
-                    </span>
-                </label>
-                <br/>
-                <span class="fld-input">
-                    <input type="text" id="quest" name="question_of_poll" size="80" maxlength="150"  value="<?php echo $question; ?>">
-                </span>
-            </div>
-        </div>
-        <?php
+	<fieldset class="frm-group group<?php echo ++$forum_page['group_count'] ?>">
+		<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
+			<div class="sf-box text">
+				<label for="fld<?php echo ++$forum_page['fld_count'] ?>">
+					<span>
+						<?php echo $lang_pun_poll['Poll question'] ?>
+					</span>
+				</label>
+				<br/>
+				<span class="fld-input">
+					<input type="text" id="quest" name="question_of_poll" size="80" maxlength="150"  value="<?php echo $question; ?>">
+				</span>
+			</div>
+		</div>
+		<?php
 
-        //Validate of pull_answers
-        if ($poll_answers != null)
-        {
-            for ($ans_num = 0; $ans_num < count($poll_answers); $ans_num++)
-                $poll_answers[$ans_num] = forum_trim($poll_answers[$ans_num]);
-            $poll_answers = array_unique($poll_answers);
-        }
+		//Validate of pull_answers
+		if ($poll_answers != null)
+		{
+			for ($ans_num = 0; $ans_num < count($poll_answers); $ans_num++)
+				$poll_answers[$ans_num] = forum_trim($poll_answers[$ans_num]);
+			$poll_answers = array_unique($poll_answers);
+		}
 
-        for ($opt_num = 0; $opt_num < $options_count; $opt_num++)
-        {
+		for ($opt_num = 0; $opt_num < $options_count; $opt_num++)
+		{
 
-        ?>
-            <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
-                <div class="sf-box text">
-                    <label for="fld<?php echo ++$forum_page['fld_count'] ?>">
-                        <span>
-                            <?php echo $lang_pun_poll['Voting answer'] ?>
-                        </span>
-                    </label>
-                    <br/>
-                    <span class="fld-input">
-                        <input id="fld<?php echo $forum_page['fld_count'] ?>" type="text" name="poll_answer[]" size="80" maxlength="70" value="<?php echo ($poll_answers != null && isset($poll_answers[$opt_num]) ? forum_htmlencode($poll_answers[$opt_num]) : '') ?>">
-                    </span>
-                </div>
-            </div>
-        <?php
+		?>
+			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
+				<div class="sf-box text">
+					<label for="fld<?php echo ++$forum_page['fld_count'] ?>">
+						<span>
+							<?php echo $lang_pun_poll['Voting answer'] ?>
+						</span>
+					</label>
+					<br/>
+					<span class="fld-input">
+						<input id="fld<?php echo $forum_page['fld_count'] ?>" type="text" name="poll_answer[]" size="80" maxlength="70" value="<?php echo ($poll_answers != null && isset($poll_answers[$opt_num]) ? forum_htmlencode($poll_answers[$opt_num]) : '') ?>">
+					</span>
+				</div>
+			</div>
+		<?php
 
-        }
+		}
 
-        ?>
-    </fieldset>
-    <fieldset class="frm-group frm-hdgroup group<?php echo ++$forum_page['group_count'] ?>">
-        <fieldset class="mf-set set<?php echo ++$forum_page['item_count'] ?> mf-head">
-            <legend>
-                <span><?php echo $lang_pun_poll['Summary count'] ?></span>
-            </legend>
-            <div class="mf-box">
-                <div class="mf-field mf-field1">
-                    <label for="fld1">
-                        <span class="fld-label"><?php echo $lang_pun_poll['Count']; ?></span>
-                    </label>
-                    <br/>
-                    <span class="fld-input">
-                        <input id="fld<?php echo ++$forum_page['fld_count'] ?>" type="text" name="poll_ans_count" size="5" maxlength="5" value="<?php echo $options_count ?>">
-                    </span>
-                </div>
-                <div class="mf-field">
-                    <span class="submit">
-                        <input type="submit" name="update_poll" value="<?php echo $lang_pun_poll['Button note'] ?>">
-                    </span>
-                </div>
-            </div>                
-        </fieldset>
-    </fieldset>
-    <fieldset class="frm-group group<?php echo ++$forum_page['group_count'] ?>">
-        <?php if ($forum_config['p_pun_poll_enable_read']): ?>
-        <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
-            <div class="sf-box checkbox">
-                <span class="fld-input">
-                    <input type="checkbox" id="first_option" value="1" name="read_unvote_users" <?php echo isset($_POST['read_unvote_users']) ? 'checked' : '' ?>/>
-                </span>
-                <label for="fld<?php echo $forum_page['fld_count'] ?>">
-                    <span><?php echo $lang_pun_poll['Show poll res'] ?></span>
-                    <?php echo $lang_pun_poll['See results'] ?>
-                </label>
-            </div>
-        </div>
-        <?php endif; ?>
-        <?php if ($forum_config['p_pun_poll_enable_revote']): ?>
-        <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
-            <div class="sf-box checkbox">
-                <span class="fld-input">
-                    <input type="checkbox" id="second_option" value="1" name="revouting" <?php echo isset($_POST['revouting']) ? 'checked' : '' ?>/>
-                </span>
-                <label for="fld<?php echo $forum_page['fld_count'] ?>">
-                    <span><?php echo $lang_pun_poll['All ch vote'] ?></span>
-                    <?php echo $lang_pun_poll['Able revote'] ?>
-                </label>
-            </div>
-        </div>
-        <?php endif; ?>
-        <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
-            <div class="sf-box text">
-                <label for="fld<?php echo ++$forum_page['fld_count'] ?>">
-                    <span>
-                        <?php echo $lang_pun_poll['Allow days'] ?>
-                    </span>
-                </label>
-                <br/>
-                <span class="fld-input">
-                    <input id="fld<?php echo $forum_page['fld_count'] ?>" type="text" name="allow_poll_days" size="5" maxlength="5" value="<?php echo $days_poll; ?>">
-                </span>
-            </div>
-        </div>
-        <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
-            <div class="sf-box text">
-                <label for="fld<?php echo ++$forum_page['fld_count'] ?>">
-                    <span>
-                        <?php echo $lang_pun_poll['Votes needed'] ?>
-                    </span>
-                </label>
-                <br/>
-                <span class="fld-input">
-                    <input id="fld<?php echo $forum_page['fld_count'] ?>" type="text" name="allow_poll_votes" size="5" maxlength="5" value="<?php echo ($votes_poll == null) ? ('') : ($votes_poll); ?>">
-                </span>
-            </div>
-        </div>
-    </fieldset>
+		?>
+	</fieldset>
+	<fieldset class="frm-group frm-hdgroup group<?php echo ++$forum_page['group_count'] ?>">
+		<fieldset class="mf-set set<?php echo ++$forum_page['item_count'] ?> mf-head">
+			<legend>
+				<span><?php echo $lang_pun_poll['Summary count'] ?></span>
+			</legend>
+			<div class="mf-box">
+				<div class="mf-field mf-field1">
+					<label for="fld1">
+						<span class="fld-label"><?php echo $lang_pun_poll['Count']; ?></span>
+					</label>
+					<br/>
+					<span class="fld-input">
+						<input id="fld<?php echo ++$forum_page['fld_count'] ?>" type="text" name="poll_ans_count" size="5" maxlength="5" value="<?php echo $options_count ?>">
+					</span>
+				</div>
+				<div class="mf-field">
+					<span class="submit">
+						<input type="submit" name="update_poll" value="<?php echo $lang_pun_poll['Button note'] ?>">
+					</span>
+				</div>
+			</div>
+		</fieldset>
+	</fieldset>
+	<fieldset class="frm-group group<?php echo ++$forum_page['group_count'] ?>">
+		<?php if ($forum_config['p_pun_poll_enable_read']): ?>
+		<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
+			<div class="sf-box checkbox">
+				<span class="fld-input">
+					<input type="checkbox" id="first_option" value="1" name="read_unvote_users" <?php echo isset($_POST['read_unvote_users']) || $read_unvote_users ? 'checked' : '' ?>/>
+				</span>
+				<label for="fld<?php echo $forum_page['fld_count'] ?>">
+					<span><?php echo $lang_pun_poll['Show poll res'] ?></span>
+					<?php echo $lang_pun_poll['See results'] ?>
+				</label>
+			</div>
+		</div>
+		<?php endif; ?>
+		<?php if ($forum_config['p_pun_poll_enable_revote']): ?>
+		<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>"">
+			<div class="sf-box checkbox">
+				<span class="fld-input">
+					<input type="checkbox" id="second_option" value="1" name="revouting" <?php echo isset($_POST['revouting']) || $revote ? 'checked' : '' ?>/>
+				</span>
+				<label for="fld<?php echo $forum_page['fld_count'] ?>">
+					<span><?php echo $lang_pun_poll['All ch vote'] ?></span>
+					<?php echo $lang_pun_poll['Able revote'] ?>
+				</label>
+			</div>
+		</div>
+		<?php endif; ?>
+		<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
+			<div class="sf-box text">
+				<label for="fld<?php echo ++$forum_page['fld_count'] ?>">
+					<span>
+						<?php echo $lang_pun_poll['Allow days'] ?>
+					</span>
+				</label>
+				<br/>
+				<span class="fld-input">
+					<input id="fld<?php echo $forum_page['fld_count'] ?>" type="text" name="allow_poll_days" size="5" maxlength="5" value="<?php echo $days_poll; ?>">
+				</span>
+			</div>
+		</div>
+		<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
+			<div class="sf-box text">
+				<label for="fld<?php echo ++$forum_page['fld_count'] ?>">
+				<span>
+						<?php echo $lang_pun_poll['Votes needed'] ?>
+					</span>
+				</label>
+				<br/>
+				<span class="fld-input">
+					<input id="fld<?php echo $forum_page['fld_count'] ?>" type="text" name="allow_poll_votes" size="5" maxlength="5" value="<?php echo ($votes_poll == null) ? ('') : ($votes_poll); ?>">
+				</span>
+			</div>
+		</div>
+	</fieldset>
 <?php
 
 }
--- pun_poll/lang/English/pun_poll.php
+++ pun_poll/lang/English/pun_poll.php
@@ -14,7 +14,7 @@ $lang_pun_poll = array(
 
 	'Poll question'					=> 'Poll question',
 	'Voting answer'					=> 'Poll option',
-	'See results'					=> 'Unvoting users can see poll results',
+	'See results'					=> 'Unvoted users can see poll results',
 	'Able revote'					=> 'Allow users to change their opinions.',
 	'Delete all'					=> 'Would you like to delete all poll results if answers will be changed (Yes/No)',
 
@@ -25,7 +25,7 @@ $lang_pun_poll = array(
 	'Disable revoting'				=> 'Allow vote change',
 	'Disable see results'			=> 'Enable read results',
 	'Maximum answers info'			=> 'Maximum answers',
-	'Disable see results info'		=> 'Unvoting users can see poll results ',
+	'Disable see results info'		=> 'Unvoted users can see poll results ',
 	'Disable revoting info'			=> 'Enable revoting',
 	'Poll add'						=> 'Allows users to use poll in their topics',
 	'Permission'					=> 'Voting permission',
@@ -42,8 +42,8 @@ $lang_pun_poll = array(
 	'All ch vote'					=> 'Allow to change voiting',
 	'Max cnt options'				=> 'Count of options can\'t be more than %s.',
 	'Min cnt options'				=> 'Count of options can\'t be less than 2',
-	'Days limit'					=> 'Count of poll days can be more than 30 and less than 1.',
-	'Votes count'					=> 'Count of votes can be more than 500 and less than 10.',
+	'Days limit'					=> 'Count of poll days can\'t be more than 30 and less than 1.',
+	'Votes count'					=> 'Count of votes can\'t be more than 500 and less than 10.',
 	'Header note'					=> 'Vouting',
 	'Options'						=> 'Vote options',
 	'Results'						=> 'Voting results:',
@@ -59,6 +59,6 @@ $lang_pun_poll = array(
 	'Empty question'				=> 'You should enter a question of poll.',
 	'Merge error'					=> 'You can\'t merge this topics, because 2 or more topics have voting. Remove voting before merging.',
 	'Edit voting'					=> 'This two options allow to you reset voting results or remove voting. If you want to edit voting send e-mail to administrator of Forum <a href="mailto:%s">%s</a>.',
-	'New voting'					=> 'You can create new voting, using this form. Number of answers can\'t be less than 2. Length of question can\'t be less than 10 symbols. If you want to add answer, enter number of answers that you needed in the field and press "Update poll". You can choose when your voting will be finished. For this you can enter count of days or maximum count of votes. Remember: if you enter count of days, count of votes will be ignored. If your administrator allow to change user vote or unvoted users can see voting results you will see appropriate options.',
+	'New voting'					=> 'Using this form, you can create a new voting. The number of possible answers can\'t be less than 2. A question\'s length can\'t be less than 5 symbols. A answer\'s length can\'t be less than 2 symbols.  If you want to add an answer, enter the number of answers you require in the corresponding field and press "Update poll". You can choose the time when your voting will be finished. For this purpose, you can enter a count of days or a maximum count of votes. Remember: if you enter the count of days, the count of votes will be ignored. If your administrator allows to change a user\'s vote or unvoted users can see voting results, you will see appropriate options.',
 	'Edit voting admin'				=> 'You can edit voting.'
 );
