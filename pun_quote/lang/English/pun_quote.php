76c3223527593d88d69020834b8d063db7c5df73
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-02-10 16:21:24 +0000

    pun_quote: The copyright message added to lang-file.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1044 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_quote/lang/English/pun_quote.php
+++ pun_quote/lang/English/pun_quote.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Lang file for pun_quote
+ *
+ * @copyright Copyright (C) 2008 PunBB
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_quote
+ */
 if (!defined('FORUM')) die();
 
 // Language definitions used in pun_quote
