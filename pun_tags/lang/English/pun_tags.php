696df0a428c4c8133620bb237e28c3827badbe95
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-05-26 13:57:18 +0000

    pun_tags: Added an option which allows to show/hide the cloud of tags.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1182 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_tags/lang/English/pun_tags.php
+++ pun_tags/lang/English/pun_tags.php
@@ -12,10 +12,12 @@ if (!defined('FORUM'))
 	die();
 
 $lang_pun_tags = array(
-	'Pun Tags'						=>	'Pun Tags:',
+	'Pun Tags'						=>	'Pun Tags',
 	'Settings'						=>	'Pun Tags settings',
 	'Topic tags'					=>	'Topic tags',
 	'Enter tags' 					=>	'Enter a few words (called tags) separated by commas to help someone find your topic.',
+	'Show Pun Tags'					=>	'Show Pun Tags',
+	'Pun Tags notice'				=>	'Display list of tags at the pages',
 	'Count error'					=>	'Total length of tags can\'t be more than 100.',
 	'Tags count'					=>	'Limit of tags cloud',
 	'Tags count info'				=>	'Maximum count of tags in the cloud.'
