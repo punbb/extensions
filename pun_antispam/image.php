a3c37ce8d5680c1d36eeeaf587d7e96036ff1694
Roman <Roman@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-06-10 10:09:46 +0000

    pun_antispam: now uses Securimage (phpcaptcha.org). Version changed (1.2 => 1.3).
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1205 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_antispam/image.php
+++ pun_antispam/image.php
@@ -3,44 +3,30 @@
 /**
  * Generates a CAPTCHA picture
  *
- * @copyright Copyright (C) 2008 PunBB, partially based on code by Jamie Furness
+ * @copyright (C) 2008-2009 PunBB
  * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  * @package pun_antispam
  */
+ 
+/*
 
-// Generate a random string
-function pun_antispam_rand_str ()
-{
-	return strtr(substr(strtolower(md5(uniqid(rand(), 1))), 2, 7), 'abcdef', '165380');
-}
+We use Securimage PHP CAPTCHA (http://www.phpcaptcha.org)
 
-// Output CAPTCHA string into an image
-function pun_antispam_image ($string)
-{
-	$im = imagecreate(100, 18);
-
-	$white = imagecolorallocate($im, 255, 255, 255);
-	$black = imagecolorallocate($im, 0, 0, 0);
-	$other = imagecolorallocate($im, 128, 128, 255);
-
-	for ($i = strlen($string); $i--; )
-		imagestring($im, rand(4, 5), 14 * $i + rand(0, 5), rand(0, 2), $string{$i}, $black);
-
-	sleep(1);
-
-	header('Content-type:image/jpeg');
-	header('Cache-Control: no-cache, must-revalidate');
-	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-	header('Pragma: no-cache');
-
-	imagejpeg($im, null, 30);
-	imagedestroy($im);
-}
+The only thing this script should do (besides to output an CAPTCHA image)
+is to set the CAPTCHA text to the session variable.
+Example:
 
 session_start();
 
-$pun_antispam_string = pun_antispam_rand_str();
-$_SESSION['pun_antispam_text'] = $pun_antispam_string;
-pun_antispam_image($pun_antispam_string);
+$_SESSION['pun_antispam_text'] = <CAPTCHA TEXT HERE>;
+
+*/
+
+include 'securimage.php';
+
+usleep(300000);
+
+$img = new securimage();
+$img->show();
 
 ?>
--- pun_antispam/lang/English/pun_antispam.php
+++ pun_antispam/lang/English/pun_antispam.php
@@ -1,19 +1,22 @@
 <?php
 
 // Language definitions used in pun_antispam
+
 $lang_pun_antispam = array(
 	'Captcha'						=>	'Captcha',
 	'Captcha Info'					=>	'Please enter the letters in the image.',
-	'img alt'						=>	'Captcha image. Turn on pictures to see it :)',
+	'img alt'						=>	'Captcha image. Turn pictures on to see it.',
 	'Invalid Text'					=>	'The captcha text you entered appears to be wrong.',
 
+	'reload image'					=>	'(reload image)',
+
 	'Captcha admin head'			=>	'Setup which actions are protected by a captcha',
-	'Captcha admin info'			=>	'You may enable captcha in certain areas of your forum if you are experiencing problems with spam.',
-	'Captcha admin legend'			=>	'Enable Captcha',
+	'Captcha admin info'			=>	'You may enable the captcha protection in certain areas of your forum if you are experiencing problems with spam.',
+	'Captcha admin legend'			=>	'Enable captcha',
 
 	'Captcha registrations info'	=>	'Require a captcha before users may register. This can be helpful to stop spam.',
-	'Captcha login info'			=>	'Require a captcha when logging in. This can be helpful to stop brute-force attacks.',
-	'Captcha reset info'			=>	'Require a captcha to be entered when a user tries to reset their password.',
+	'Captcha login info'			=>	'Require a captcha when logging in. Can help stopping brute-force attacks.',
+	'Captcha reset info'			=>	'Require a captcha to be entered when users try to reset their passwords.',
 	'Captcha guestpost info'		=>	'Require a captcha for guest posting (if enabled one).'
 );
 
