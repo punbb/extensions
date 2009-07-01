74eabdb07f61f71496f0db5b49ca772e598deb9c
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-07-01 14:23:12 +0000

    pun_attachment: format, markup fixes on the page with attachment settings.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1214 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_attachment/pun_attach.php
+++ pun_attachment/pun_attach.php
@@ -30,26 +30,25 @@ foreach ($names as $icon_name)
 	}
 }
 
-	// Setup the form
-	$forum_page['group_count'] = $forum_page['item_count'] = $forum_page['fld_count'] = 0;
+// Setup the form
+$forum_page['group_count'] = $forum_page['item_count'] = $forum_page['fld_count'] = 0;
 
-	// Setup breadcrumbs
-	$forum_page['crumbs'] = array(
-		array($forum_config['o_board_title'], forum_link($forum_url['index'])),
-		array($lang_admin_common['Forum administration'], forum_link($forum_url['admin_index'])),
-		array($lang_admin_common['Settings'], forum_link($forum_url['admin_settings_setup'])),
-		array($lang_attach['Attachment'], forum_link($attach_url['admin_options_attach']))		
-	);
-	
-	define('FORUM_PAGE_SECTION', 'settings');
-	define('FORUM_PAGE', 'admin-options-attach');
-	require FORUM_ROOT.'header.php';
+// Setup breadcrumbs
+$forum_page['crumbs'] = array(
+	array($forum_config['o_board_title'], forum_link($forum_url['index'])),
+	array($lang_admin_common['Forum administration'], forum_link($forum_url['admin_index'])),
+	array($lang_admin_common['Settings'], forum_link($forum_url['admin_settings_setup'])),
+	array($lang_attach['Attachment'], forum_link($attach_url['admin_options_attach']))
+);
 
-	// START SUBST - <!-- forum_main -->
-	ob_start();	
+define('FORUM_PAGE_SECTION', 'settings');
+define('FORUM_PAGE', 'admin-options-attach');
+require FORUM_ROOT.'header.php';
+
+// START SUBST - <!-- forum_main -->
+ob_start();	
 
 ?>
-
 <div class="main-content main-frm">
 	<form class="frm-form" method="post" accept-charset="utf-8" action="<?php echo forum_link($attach_url['admin_options_attach']) ?>">
 		<div class="content-head">
@@ -62,25 +61,25 @@ foreach ($names as $icon_name)
 		<fieldset class="frm-group group1">
 			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 				<div class="sf-box checkbox">
-					<span class="fld-input"><input type="checkbox" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[disable_attach]" value="1"<?php if ($forum_config['attach_disable_attach'] == '1') echo ' checked="checked"' ?> /></span>
-					<label for="fld<?php echo $forum_page['fld_count'] ?>"><span><?php echo 'Disable attachment' ?></span><?php echo $lang_attach['Disable attachments'] ?></label>					
+					<span class="fld-input"><input type="checkbox" id="fld<?php echo ++$forum_page['fld_count']; ?>" name="form[disable_attach]" value="1"<?php if ($forum_config['attach_disable_attach'] == '1') echo ' checked="checked"' ?> /></span>
+					<label for="fld<?php echo $forum_page['fld_count'] ?>"><span><?php echo 'Disable attachment' ?></span><?php echo $lang_attach['Disable attachments'] ?></label>
 				</div>
 			</div>
 			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 				<div class="sf-box checkbox">
-					<span class="fld-input"><input type="checkbox" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[create_orphans]" value="1"<?php if ($forum_config['attach_create_orphans'] == '1') echo ' checked="checked"' ?> /></span>
+					<span class="fld-input"><input type="checkbox" id="fld<?php echo ++$forum_page['fld_count']; ?>" name="form[create_orphans]" value="1"<?php if ($forum_config['attach_create_orphans'] == '1') echo ' checked="checked"' ?> /></span>
 					<label for="fld<?php echo $forum_page['fld_count'] ?>"><span><?php echo 'Create orphans' ?></span><?php echo $lang_attach['Orphans help'] ?></label>
 				</div>
 			</div>
 			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 				<div class="sf-box text">
-					<label for="fld<?php echo ++$forum_page['fld_count'] ?>"><span><?php echo $lang_attach['Always deny'] ?></span></label><br />				
-					<span class="fld-input"><input type="text" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[always_deny]" size="75" maxlength="150" value="<?php echo htmlspecialchars($forum_config['attach_always_deny']); ?>" /></span>				
+					<label for="fld<?php echo ++$forum_page['fld_count'] ?>"><span><?php echo $lang_attach['Always deny'] ?></span></label><br />
+					<span class="fld-input"><input type="text" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[always_deny]" size="75" maxlength="150" value="<?php echo htmlspecialchars($forum_config['attach_always_deny']); ?>" /></span>
 				</div>
 			</div>
 			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 				<div class="sf-box checkbox">
-					<span class="fld-input"><input type="checkbox" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[disp_small]" value="1"<?php if ($forum_config['attach_disp_small'] == '1') echo ' checked="checked"' ?> /></span>
+					<span class="fld-input"><input type="checkbox" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="form[disp_small]" value="1"<?php if ($forum_config['attach_disp_small'] == '1') echo ' checked="checked"' ?> /></span>
 					<label for="fld<?php echo $forum_page['fld_count'] ?>"><span><?php echo 'Display small images' ?></span><?php echo $lang_attach['Display small'] ?></label>
 				</div>
 			</div>
@@ -89,7 +88,7 @@ foreach ($names as $icon_name)
 					<label for="fld<?php echo ++$forum_page['fld_count'] ?>"><span><?php echo $lang_attach['Max height'] ?></span></label><br />
 					<span class="fld-input"><input type="text" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[small_height]" size="5" maxlength="5" value="<?php echo htmlspecialchars($forum_config['attach_small_height']); ?>" /></span>
 				</div>
-			</div>			
+			</div>
 			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 				<div class="sf-box text">
 					<label for="fld<?php echo ++$forum_page['fld_count'] ?>"><span><?php echo $lang_attach['Max width'] ?></span></label><br />
@@ -121,7 +120,6 @@ foreach ($names as $icon_name)
 		if (!empty($pun_attach_errors['big_images']))
 		{
 
-
 ?>
 			<h2 class="warn hn"><?php echo $lang_attach['Big icons'] ?></h2>
 			<ul class="error-list">
@@ -137,25 +135,26 @@ foreach ($names as $icon_name)
 		<fieldset class="frm-group group<?php echo ++$forum_page['group_count'] ?>">
 			<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 				<div class="sf-box checkbox">
-					<span class="fld-input"><input type="checkbox" id="fld<?php echo $forum_page['fld_count'] ?>" name="form[use_icon]" value="1"<?php if ($forum_config['attach_use_icon'] == '1') echo ' checked="checked"' ?> /></span>
+					<span class="fld-input"><input type="checkbox" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="form[use_icon]" value="1"<?php if ($forum_config['attach_use_icon'] == '1') echo ' checked="checked"' ?> /></span>
 					<label for="fld<?php echo $forum_page['fld_count'] ?>"><span><?php echo $lang_attach['Use icons'] ?></span><?php echo $lang_attach['Display icons'] ?></label>
 				</div>
 			</div>
 <?php
 
-			if (isset($names) && isset($icons))
+			if (!empty($names) && !empty($icons))
 			{
-				for ($i=0; $i < count($icons); $i++)
+				for ($i = 0; $i < count($icons); $i++)
 				{
 
 ?>
 						<div class="sf-box text">
-							<label for="fld<?php echo ++$forum_page['fld_count'] ?>"><span></span></label><br />
-								<span class="fld-input">
-									<input type="text" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="attach_ext_<?php echo $i ?>" size="10" maxlength="10" value="<?php echo (isset($_POST['attach_ext_'.$i]) ? htmlspecialchars($_POST['attach_ext_'.$i]) : $icons[$i]) ?>" />
-									<input type="text" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="attach_ico_<?php echo $i ?>" size="10" maxlength="10" value="<?php echo (isset($_POST['attach_ico_'.$i]) ? htmlspecialchars($_POST['attach_ico_'.$i]) : $names[$i]) ?>" />
-									<span class="fld-input"><img src="<?php echo (!(in_array($forum_config['attach_icon_folder'].$names[$i],$big_images) || in_array($forum_config['attach_icon_folder'].$names[$i],$missing_files)) ? $forum_config['attach_icon_folder'].$names[$i] : '') ?>" /></span>
-								</span>
+							<span class="fld-input">
+								<input type="text" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="attach_ext_<?php echo $i ?>" size="10" maxlength="10" value="<?php echo (isset($_POST['attach_ext_'.$i]) ? htmlspecialchars($_POST['attach_ext_'.$i]) : $icons[$i]) ?>" />
+								<input type="text" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="attach_ico_<?php echo $i ?>" size="10" maxlength="10" value="<?php echo (isset($_POST['attach_ico_'.$i]) ? htmlspecialchars($_POST['attach_ico_'.$i]) : $names[$i]) ?>" />
+								<?php if (!in_array($forum_config['attach_icon_folder'].$names[$i], $big_images) && !in_array($forum_config['attach_icon_folder'].$names[$i], $missing_files)): ?>
+								<span class="fld-input"><img src="<?php echo $forum_config['attach_icon_folder'].$names[$i]; ?>" alt="<?php echo $names[$i]; ?>" /></span>
+								<?php endif; ?>
+							</span>
 						</div>
 
 <?php
@@ -166,7 +165,6 @@ foreach ($names as $icon_name)
 ?>
 				<div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
 					<div class="sf-box text">
-						<label for="fld<?php echo ++$forum_page['fld_count'] ?>"><span></span></label>
 						<span class="fld-input">
 							<input type="text" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="add_field_icon" size="10" maxlength="10" />
 							<input type="text" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="add_field_file" size="10" maxlength="10" />
@@ -177,5 +175,5 @@ foreach ($names as $icon_name)
 			<div class="frm-buttons">
 				<span class="submit"><input type="submit" name="update_settings" value="<?php echo $lang_admin_common['Save changes'] ?>" /></span>
 			</div>
-		</form>		
+		</form>
 	</div>
