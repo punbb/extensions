4dbd8d90d4beee1eb83b2b7c8cb9b4bf18db11b3
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-07-15 11:11:20 +0000

    pun_attachment: removing attachments during the uninstallation process was added. Langization of the extension was updated.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1238 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_attachment/pun_list_attach.php
+++ pun_attachment/pun_list_attach.php
@@ -10,7 +10,10 @@
 
 if (!defined('FORUM')) die();
 
-require $ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php';
+if (file_exists($ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php'))
+	require $ext_info['path'].'/lang/'.$forum_user['language'].'/'.$ext_info['id'].'.php';
+else
+	require $ext_info['path'].'/lang/English/'.$ext_info['id'].'.php';
 
 if (isset($_GET['id']))
 	require $ext_info['path'].'/pun_attachment_page.php';
