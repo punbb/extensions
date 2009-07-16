ac47b07613cbf023e2eb0b29a42587751d10597c
mikhail <mikhail@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-07-16 19:54:41 +0000

    pun_bbcode: finished the ticket #263.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1249 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_bbcode/bar.php
+++ pun_bbcode/bar.php
@@ -1,33 +1,60 @@
 <?php
 
-/**
- * pun_bbcode bar with buttons and smilies
- *
- * @copyright (C) 2008-2009 PunBB
- * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
- * @package pun_bbcode
- */
+/***********************************************************************
+
+	Copyright (C) 2008  PunBB
+
+	Based on Easy BBCode extension by Rickard Andersson.
+
+	PunBB is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published
+	by the Free Software Foundation; either version 2 of the License,
+	or (at your option) any later version.
+
+	PunBB is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+	MA  02111-1307  USA
+
+***********************************************************************/
 
 if (!defined('FORUM'))
-	die();
+    die();
 
+ob_end_clean();
+ob_start();
 // NOTE: I couldn't find how to remove sf-set from here.
 ?>
-				<div class="sf-set" id="pun_bbcode_bar">
-					<div id="pun_bbcode_wrapper"<?php echo $forum_user['pun_bbcode_use_buttons']?' class="graphical"':'' ?>>
-						<div id="pun_bbcode_buttons">
+		<div id="pun_bbcode_wrapper"<?php echo (($forum_user['pun_bbcode_use_buttons']) && ($forum_config['p_message_bbcode']))?' class="graphical"':'' ?>>
+<?php
+		$tabindex = -1;
+		
+		if ($forum_config['p_message_bbcode'])
+		{
+?>
+			<div id="pun_bbcode_buttons">
 <?php
 
 // List of tags, which may have attribute
-$tags_without_attr = array('b', 'i', 'u', 'url', 'email', 'img', 'list', 'li' => '*', 'quote', 'code');
+$tags_without_attr = array('b', 'i', 'u', 'email', 'list', 'li' => '*', 'quote', 'code', 'url');
+
+if ($forum_config['p_message_img_tag'])
+	$tags_without_attr[] = 'img';
 
 // List of tags, which may not to have attribute
 if ($forum_user['pun_bbcode_use_buttons'])
-	$tags_with_attr = array('color');
+    $tags_with_attr = array('color');
 else
-	$tags_with_attr = array('quote', 'color', 'url', 'email', 'img', 'list');
+    $tags_with_attr = array('quote', 'color', 'url', 'email', 'list');
 
-($hook = get_hook('pun_bbcode_pre_tags_merge')) ? eval($hook) : null;
+if (!$forum_user['pun_bbcode_use_buttons'])
+	if ($forum_config['p_message_img_tag'])
+		$tags_with_attr[] = 'img';
 
 // Let's get the list of all tags
 $tags = array_unique(array_merge($tags_without_attr, $tags_with_attr));
@@ -40,53 +67,46 @@ if ($forum_user['pun_bbcode_use_buttons'])
 		$buttons_path = $ext_info['url'].'/buttons/Oxygen';
 }
 
-$pun_bbcode_tabindex = 1;
-
-($hook = get_hook('pun_bbcode_pre_buttons_output')) ? eval($hook) : null;
 
 foreach ($tags as $filename => $tag)
 {
-	($hook = get_hook('pun_bbcode_buttons_output_loop_start')) ? eval($hook) : null;
-
 	if (in_array($tag, $tags_without_attr))
 	{
 		if ($forum_user['pun_bbcode_use_buttons'])
-			echo "\t\t\t\t\t\t\t".'<img src="'.$buttons_path.'/'.(is_numeric($filename) ? $tag : $filename).'.png" alt="['.$tag.']" title="'.$tag.'"';
+			echo '<img src="'.$buttons_path.'/'.(is_numeric($filename)?$tag:$filename).'.png" alt="['.$tag.']" title="'.$tag.'"';
 		else
-			echo "\t\t\t\t\t\t\t".'<input type="button" value="'.ucfirst($tag).'" name="'.$tag.'"';
+			echo '<input type="button" value="'.ucfirst($tag).'" name="'.$tag.'"';
 
-		echo ' onclick="insert_text(\'['.$tag.']\',\'[/'.$tag.']\')" tabindex="'.$pun_bbcode_tabindex.'"/>'."\n";
+		echo ' onclick="insert_text(\'['.$tag.']\',\'[/'.$tag.']\')" tabindex="'.$tabindex.'"/>';
 	}
 
 	if (in_array($tag, $tags_with_attr))
 	{
 		if ($forum_user['pun_bbcode_use_buttons'])
-			echo "\t\t\t\t\t\t\t".'<img src="'.$buttons_path.'/'.(is_numeric($filename)?$tag:$filename).'.png" alt="['.$tag.'=]" title="'.$tag.'="';
+			echo '<img src="'.$buttons_path.'/'.(is_numeric($filename)?$tag:$filename).'.png" alt="['.$tag.'=]" title="'.$tag.'="';
 		else
-			echo "\t\t\t\t\t\t\t".'<input type="button" value="'.ucfirst($tag).'=" name="'.$tag.'"';
+			echo '<input type="button" value="'.ucfirst($tag).'=" name="'.$tag.'"';
 
-		echo ' onclick="insert_text(\'['.$tag.'=]\',\'[/'.$tag.']\')" tabindex="'.$pun_bbcode_tabindex.'" />'."\n";
+		echo ' onclick="insert_text(\'['.$tag.'=]\',\'[/'.$tag.']\')" tabindex="'.$tabindex.'" />';
 	}
 
-	$pun_bbcode_tabindex++;
+	$tabindex--;
 }
 
 ?>
-						</div>
-						<div id="pun_bbcode_smilies">
+			</div>
+			<?php
+		}
+		?>
+			<div id="pun_bbcode_smilies">
+			<?php
+			// Display the smiley set
+			foreach (array_unique($smilies) as $smile_text => $smile_file)
+				echo '<a href="javascript:insert_text(\' '.$smile_text.' \', \'\');" tabindex="'.($tabindex--).'"><img src="'.$base_url.'/img/smilies/'.$smile_file.'" width="15" height="15" alt="'.$smile_text.'" /></a>'."\n";
+			?>
+			</div>
+		</div>
 <?php
-
-($hook = get_hook('pun_bbcode_pre_smilies_output')) ? eval($hook) : null;
-
-// Display the smiley set
-foreach (array_unique($smilies) as $smile_text => $smile_file)
-{
-	($hook = get_hook('pun_bbcode_smilies_output_loop_start')) ? eval($hook) : null;
-
-	echo "\t\t\t\t\t\t\t".'<img src="'.$base_url.'/img/smilies/'.$smile_file.'" width="15" height="15" alt="'.$smile_text.'" onclick="insert_text(\''.$smile_text.'\', \'\');" tabindex="'.($pun_bbcode_tabindex++).'" />'."\n";
-}
-
+$bbar_temp = str_replace(array("\n", '"'), array('\n', '\"'), forum_trim(ob_get_contents()));
+ob_end_clean();
 ?>
-						</div>
-					</div>
-				</div>
