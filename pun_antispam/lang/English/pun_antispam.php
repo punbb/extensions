0263aa0079ae5dacc3a0895b503b2eefa8c5654b
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-09-03 08:24:24 +0000

    pun_antispam: user can add signature if only he/she wrote N posts now.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1308 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_antispam/lang/English/pun_antispam.php
+++ pun_antispam/lang/English/pun_antispam.php
@@ -17,7 +17,11 @@ $lang_pun_antispam = array(
 	'Captcha registrations info'	=>	'Require a captcha before users may register. This can be helpful to stop spam.',
 	'Captcha login info'			=>	'Require a captcha when logging in. Can help stopping brute-force attacks.',
 	'Captcha reset info'			=>	'Require a captcha to be entered when users try to reset their passwords.',
-	'Captcha guestpost info'		=>	'Require a captcha for guest posting (if enabled one).'
+	'Captcha guestpost info'		=>	'Require a captcha for guest posting (if enabled one).',
+
+	'Signature info'				=>	'You should have more than %s posts to add a signature.',
+	'Min posts for sig'				=>	'Minimum posts',
+	'Min posts for sig info'		=>	'The minimum count of posts before a user can add a signature to his posts.'
 );
 
 ?>
