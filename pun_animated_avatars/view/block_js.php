32ce6748c4fc4601206dd1bf7b36f8e777d52541
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-10-15 11:54:55 +0000

    pun_animated_avatars: fixed some errors in JavaScript.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1391 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_animated_avatars/view/block_js.php
+++ pun_animated_avatars/view/block_js.php
@@ -14,20 +14,20 @@ if (!$pho_to_templates['AET']['error']): ?>
 	head_notice.className = 'hn ct-legend';
 	head_notice.innerHTML = '<?php echo $lang_pun_animated_avatars['Template preview'] ?>';
 
-	function updateFETPreview()
+	function updateAETPreview()
 	{
-		var images = new Array(<?php echo $forum_page['preview_fet_templates_str']; ?>);
-		var fet_template = document.getElementById('fet_template');
+		var images = new Array(<?php echo $forum_page['preview_aet_templates_str']; ?>);
+		var aet_template = document.getElementById('aet_template');
 
-		document.getElementById('fet_template_image').src = images[fet_template.selectedIndex];
+		document.getElementById('aet_template_image').src = images[aet_template.selectedIndex];
 	}
-	var fet_div = document.getElementById('div_fet');;
-	Forum.addEvent(document.getElementById('fet_template'), 'change', updateFETPreview, false);
-	var imgElemFet = document.createElement('img');
-	imgElemFet.id = 'fet_template_image';
+	var aet_div = document.getElementById('div_aet');;
+	Forum.addEvent(document.getElementById('aet_template'), 'change', updateAETPreview, false);
+	var imgElemAet = document.createElement('img');
+	imgElemAet.id = 'aet_template_image';
 
-	fet_div.appendChild(head_notice);
-	fet_div.appendChild(imgElemFet);
-	updateFETPreview();
+	aet_div.appendChild(head_notice);
+	aet_div.appendChild(imgElemAet);
+	updateAETPreview();
 </script>
 <?php endif; ?>
