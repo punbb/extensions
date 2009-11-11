65a58611d453be8534b3efe10fda7e67f627045a
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-11-11 15:21:40 +0000

    pun_stop_bots: initial commit.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1440 4dc816b1-62af-4023-b237-cae3452c1eb1

---
+++ pun_stop_bots/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * pun_stop_bots functions file
+ *
+ * @copyright Copyright (C) 2008 PunBB, partially based on code copyright (C) 2008 FluxBB.org
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_stop_bots
+ */
+
+if (!defined('FORUM')) die();
+
+?>
---
+++ pun_stop_bots/lang/English/pun_stop_bots.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Pun stop bots extension language file
+ *
+ * @copyright Copyright (C) 2008 PunBB, partially based on code copyright (C) 2008 FluxBB.org
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_stop_bots
+ */
+
+if (!defined('FORUM')) die();
+
+$lang_pun_stop_bots = array(
+
+);
+
+?>
