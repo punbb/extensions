c56ee45f4187693a4aed6396c55e9af79513d90e
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-11-11 16:28:56 +0000

    pun_stop_bots: added some default questions. Added function for cache creating.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1441 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_stop_bots/functions.php
+++ pun_stop_bots/functions.php
@@ -10,4 +10,36 @@
 
 if (!defined('FORUM')) die();
 
+function pun_stop_bots_generate_cache()
+{
+	global $forum_db;
+
+	// Get the forum config from the DB
+	$query = array(
+		'SELECT'	=> 'id, question, answers',
+		'FROM'		=> 'pun_stop_bots_questions'
+	);
+	$result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
+
+	$output = array();
+	if ($forum_db->num_rows($result) > 0)
+	{
+		while ($cur_item = $forum_db->fetch_row($result))
+		{
+			$output['id'] = $cur_item['id'];
+			$output['question'] = $cur_item['question'];
+			$output['answers'] = $cur_item['answers'];
+		}
+	}
+
+	// Output config as PHP code
+	$fh = @fopen(FORUM_CACHE_DIR.'cache_pun_stop_bots.php', 'wb');
+	if (!$fh)
+		error('Unable to write configuration cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'.', __FILE__, __LINE__);
+
+	fwrite($fh, '<?php'."\n\n".'define(\'PUN_STOP_BOTS_CACHE_LOADED\', 1);'."\n\n".'$pun_stop_bots_questions = '.var_export($output, true).';'."\n\n".'?>');
+
+	fclose($fh);
+}
+
 ?>
