ed369a9c450588e90cb60eac4564f15cc46ef541
GordeiS <GordeiS@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-11-16 14:09:03 +0000

    pun_move_posts_extension: minor fixes. Renamed some variables.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1448 4dc816b1-62af-4023-b237-cae3452c1eb1

---
+++ pun_move_posts_extension/lang/English/pun_move_posts.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Pun Move Posts extension language file
+ *
+ * @copyright Copyright (C) 2009 PunBB, partially based on code copyright (C) 2009 FluxBB.org
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_move_posts
+ */
+
+if (!defined('FORUM')) die();
+
+$lang_pun_move_posts = array(
+	'Destination topic'		=> 'Destination topic',
+	'Move selected'			=> 'Move selected posts',
+	'Move posts'			=> 'Move posts'
+);
+?>
