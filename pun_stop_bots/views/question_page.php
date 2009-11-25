64dad7080321b6e8fbfa6e967b89287e0ace18c2
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-11-25 11:46:39 +0000

    pun_stop_bots: added page with question.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1454 4dc816b1-62af-4023-b237-cae3452c1eb1

---
+++ pun_stop_bots/views/question_page.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * pun_stop_bots view of question page
+ *
+ * @copyright Copyright (C) 2008 PunBB, partially based on code copyright (C) 2008 FluxBB.org
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_stop_bots
+ */
+
+?>
+<div class="main-head">
+	<h2 class="hn"><span><?php echo $lang_pun_stop_bots['Stop bots question']; ?></span></h2>
+</div>
+<div class="main-subhead">
+	<h2 class="hn"><span><?php echo $forum_page['question']; ?> </span></h2>
+</div>
+<div class="main-content main-frm">
+	<form id="afocus" class="frm-form" method="post" accept-charset="utf-8" action="<?php echo $forum_page['form_handler']; ?>">
+		<div class="hidden">
+		<?php foreach ($forum_page['hidden_fields'] as $hidden_key => $hidden_value) {?>
+			<input name="<?php echo $hidden_key; ?>" value="<?php echo $hidden_value; ?>" type="hidden">
+		<?php } ?>
+		</div>
+		<div class="sf-set set1">
+			<div class="sf-box text">
+				<label for="fld1">
+				<span><?php echo $lang_pun_stop_bots['Your answer']; ?></span>
+				</label><br/>
+				<span class="fld-input"><input type="text" value="" maxlength="255" size="50" name="pun_stop_bots_answer" id="fld1"/></span>
+			</div>
+		</div>
+		<div class="frm-buttons">
+			<span class="submit"><input name="pun_stop_bots_submit" value="<?php echo $lang_pun_stop_bots['Answer']; ?>" type="submit"></span>
+		</div>
+	</form>
+</div>
