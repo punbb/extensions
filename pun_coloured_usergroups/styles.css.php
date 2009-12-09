403f80a1f7c5d3bfbbfbfa319fdda015766e404d
GordeiS <GordeiS@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-12-09 07:22:46 +0000

    pun_colored_usergroups:continue of extension renaming.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1469 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_coloured_usergroups/styles.css.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-type: text/css; charset: UTF-8');
-define('FORUM_ROOT', '../../');
-require FORUM_ROOT.'include/essentials.php';
-require_once FORUM_CACHE_DIR.'cache_pun_coloured_usergroups.php';
-echo $pun_colored_usergroups_cache
-?>
