cc9a4edb639dd96b09965814c24b03bb040edf1a
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-12-09 07:52:34 +0000

    pun_colored_usergroups: extension id updated, copyright messages updated, format fixes.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1470 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_colored_usergroups/main.php
+++ pun_colored_usergroups/main.php
@@ -1,4 +1,15 @@
 <?php
+
+/**
+ * pun_colored_usergroups functions file
+ *
+ * @copyright (C) 2008-2009 PunBB
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_colored_usergroups
+ */
+
+if (!defined('FORUM')) die();
+
 function cache_pun_coloured_usergroups()
 {
 	global $forum_db;
@@ -8,20 +19,21 @@ function cache_pun_coloured_usergroups()
 		'FROM'		=> 'groups AS g'
 	);
 
-        $result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
+	$result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
 	$output = array();
 
-        while ($all_groups = $forum_db->fetch_assoc($result))
+	while ($all_groups = $forum_db->fetch_assoc($result))
 	{
-            $output[] =     '.group_color_'.$all_groups['g_id'].' a:link {color: '.$all_groups['link_color'].';}'."\n".'.group_color_'.$all_groups['g_id'].' a:visited {color: '.$all_groups['link_color'].';}'."\n".'.group_color_'.$all_groups['g_id'].' a:hover {color: '.$all_groups['hover_color'].';}' ."\n".'.brd .group_color_'.$all_groups['g_id'].' {color: '.$all_groups['hover_color'].';}' ."\n".'#brd-main .group_color_'.$all_groups['g_id'].' {font-size:12px; position:static; visibility:visible; color: '.$all_groups['link_color'].';}'."\n\n";
+		$output[] = '.group_color_'.$all_groups['g_id'].' a:link {color: '.$all_groups['link_color'].';}'."\n".'.group_color_'.$all_groups['g_id'].' a:visited {color: '.$all_groups['link_color'].';}'."\n".'.group_color_'.$all_groups['g_id'].' a:hover {color: '.$all_groups['hover_color'].';}' ."\n".'.brd .group_color_'.$all_groups['g_id'].' {color: '.$all_groups['hover_color'].';}' ."\n".'#brd-main .group_color_'.$all_groups['g_id'].' {font-size:12px; position:static; visibility:visible; color: '.$all_groups['link_color'].';}'."\n\n";
 	}
 
 	$fh = @fopen(FORUM_CACHE_DIR.'cache_pun_coloured_usergroups.php', 'wb');
 	if (!$fh)
 		error('Unable to write configuration cache file to cache directory. Please make sure PHP has write access to the directory \'cache\'.', __FILE__, __LINE__);
 
-	fwrite($fh, '<?php'."\n\n".'$pun_colored_usergroups_cache=\''.implode(" ",$output)."';\n\n".'?>');
+	fwrite($fh, '<?php'."\n\n".'$pun_colored_usergroups_cache = \''.implode(" ",$output)."';\n\n".'?>');
 
 	fclose($fh);
 }
+
 ?>
--- pun_colored_usergroups/styles.css.php
+++ pun_colored_usergroups/styles.css.php
@@ -1,7 +1,19 @@
 <?php
+
+/**
+ * pun_colored_usergroups css file
+ *
+ * @copyright (C) 2008-2009 PunBB
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_colored_usergroups
+ */
+
 header('Content-type: text/css; charset: UTF-8');
 define('FORUM_ROOT', '../../');
+
 require FORUM_ROOT.'include/essentials.php';
 require_once FORUM_CACHE_DIR.'cache_pun_coloured_usergroups.php';
-echo $pun_colored_usergroups_cache
+
+echo $pun_colored_usergroups_cache;
+
 ?>
