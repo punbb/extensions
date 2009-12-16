fa3e1859651fe4629ab2a0fd7159197ef60f7df1
alexp <alexp@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-12-16 13:48:06 +0000

    pun_approval: hyper links changed in order to pass page validation test.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1486 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_approval/post_app_url.php
+++ pun_approval/post_app_url.php
@@ -1,20 +1,20 @@
 <?php
 
 $post_app_url = array(
-	'Section'						=> 'admin/extensions.php?section=approvalpost&topics=1',
-	'Posts section'					=> 'admin/extensions.php?section=approvalpost&all_post=1',
-	'Topics section'				=> 'admin/extensions.php?section=approvalpost&topics=1',
-	'Users section'					=> 'admin/extensions.php?section=approvalpost&users=1',
-	'Users section redirect'		=> 'admin/extensions.php?section=approvalpost&users=1&action=$1',
-	'Permalink topic'				=> 'admin/extensions.php?section=approvalpost&topics=1&aptid=$1',
-	'topic_new_posts'				=> 'admin/extensions.php?section=approvalpost&topics=1&aptid=$1&amp;action=new',
-	'Permalink post'				=> 'admin/extensions.php?section=approvalpost&topics=1&appid=$1#p$1',
+	'Section'						=> 'admin/extensions.php?section=approvalpost&amp;topics=1',
+	'Posts section'					=> 'admin/extensions.php?section=approvalpost&amp;all_post=1',
+	'Topics section'				=> 'admin/extensions.php?section=approvalpost&amp;topics=1',
+	'Users section'					=> 'admin/extensions.php?section=approvalpost&amp;users=1',
+	'Users section redirect'		=> 'admin/extensions.php?section=approvalpost&users=1&amp;action=$1',
+	'Permalink topic'				=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;aptid=$1',
+	'topic_new_posts'				=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;aptid=$1&amp;action=new',
+	'Permalink post'				=> 'admin/extensions.php?section=approvalpost&&amp;topics=1&amp;appid=$1#p$1',
 	'bann'							=> 'admin/bans.php?add_ban=$1',
-	'delete sev'					=> 'admin/extensions.php?section=approvalpost&topics=1&del_sev=1',
-	'delete'						=> 'admin/extensions.php?section=approvalpost&topics=1&del=$1',
-	'delete all'					=> 'admin/extensions.php?section=approvalpost&topics=1&del_all=1',
-	'app sev'						=> 'admin/extensions.php?section=approvalpost&topics=1&app_sev=1',
-	'app all'						=> 'admin/extensions.php?section=approvalpost&topics=1&app_all=1',
-	'approval'						=> 'admin/extensions.php?section=approvalpost&topics=1&app=$1',
-	'Page url'						=> 'admin/extensions.php?section=approvalpost&topics=1&page=$1'
+	'delete sev'					=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;del_sev=1',
+	'delete'						=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;del=$1',
+	'delete all'					=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;del_all=1',
+	'app sev'						=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;app_sev=1',
+	'app all'						=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;app_all=1',
+	'approval'						=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;app=$1',
+	'Page url'						=> 'admin/extensions.php?section=approvalpost&amp;topics=1&amp;page=$1'
 );
