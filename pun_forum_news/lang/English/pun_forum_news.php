811ab3a76fcda15ebf64f1560103d7c2d8f87cc3
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2010-02-02 14:33:02 +0000

    pun_forum_news: added the base for new extension.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1554 4dc816b1-62af-4023-b237-cae3452c1eb1

---
+++ pun_forum_news/lang/English/pun_forum_news.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Language file for pun_forum_news extension
+ *
+ * @copyright (C) 2010 PunBB
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_forum_news
+ */
+
+
+if (!defined('FORUM')) die();
+
+
+$lang_pun_forum_news = array(
+
+);
+
+?>
