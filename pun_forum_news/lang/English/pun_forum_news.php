ec111e50d2fb6e0896be60409f487bac9e049114
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2010-02-03 14:54:26 +0000

    pun_forum_news: database structure updated. Added possibility to mark topics/posts as "news".
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1559 4dc816b1-62af-4023-b237-cae3452c1eb1

--- pun_forum_news/lang/English/pun_forum_news.php
+++ pun_forum_news/lang/English/pun_forum_news.php
@@ -14,7 +14,8 @@ if (!defined('FORUM')) die();
 
 $lang_pun_forum_news = array(
 	'Permission legend'		=> 	'Forum news',
-	'Permission text'		=>	'Allow users to add forum news'
+	'Permission text'		=>	'Allow users to add forum news',
+	'Post mark'				=>	'Mark as news',
 );
 
 ?>
