6366c71bf2c59aa12b803c6696d0375133cfdca8
alexp <alexp@4dc816b1-62af-4023-b237-cae3452c1eb1>
2010-02-09 17:01:58 +0000

    pun_invitation_only: option which allows to enable/disable invitation system added to administration->Settings->Features menu.
    git-svn-id: http://punbb.informer.com/svn/additions/punbb-1.3/extensions@1574 4dc816b1-62af-4023-b237-cae3452c1eb1

---
+++ pun_invitation_only/lang/English/pun_invitation_only.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('FORUM')) die();
+//language definitions used for invitation system (inv_sys).
+$lang_inv_sys= array(
+	//Administration->Settings->Features
+	'Title' => 'Invitation system',
+	'Enable inv title' =>'Enable invitation system',
+	'Enable inv'=> 'Allow users to send emails to their friends with invitation to register on the forum',
+
+);
+
+?>
