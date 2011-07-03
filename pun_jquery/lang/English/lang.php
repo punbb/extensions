313a83c9836fc24a8dc11256d442f51ac12b0028
dimkalinux <dimka.linux@gmail.com>
2011-07-03 19:39:03 +0300

    Add pun_jquery to repo

---
+++ pun_jquery/lang/English/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('FORUM'))
+	die();
+
+$lang_pun_jquery = array(
+	'Setup jquery'						=> 'jQuery (version %s)',
+	'Setup jquery legend'				=> 'jQuery (version %s)',
+
+	'Include method'					=> 'Include method',
+
+	'Include method local label'		=> 'Local',
+	'Include method google label'		=> 'Google Ajax API CDN ',
+	'Include method microsoft label'	=> 'Microsoft CDN',
+	'Include method jquery label'		=> 'jQuery CDN',
+);
+
+?>
