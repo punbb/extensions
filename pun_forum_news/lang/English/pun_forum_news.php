c18e1a54325892a97e8c1d8565bb30ce76ebf7ce
dimkalinux <dimka.linux@gmail.com>
2011-07-19 11:41:53 +0300

    pun_forum_news: add support for PunBB 1.4

--- pun_forum_news/lang/English/pun_forum_news.php
+++ pun_forum_news/lang/English/pun_forum_news.php
@@ -3,7 +3,7 @@
 /**
  * Language file for pun_forum_news extension
  *
- * @copyright (C) 2010 PunBB
+ * @copyright (C) 2010-2011 PunBB
  * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  * @package pun_forum_news
  */
@@ -15,7 +15,7 @@ if (!defined('FORUM')) die();
 $lang_pun_forum_news = array(
 	'Permission legend'		=> 	'Forum news',
 	'Permission text'		=>	'Allow users to add forum news',
-	'Post mark'				=>	'Mark as news',
+	'Post mark'				=>	'Mark as news.',
 	'Forum news'			=>	'News',
 	'No news'				=>	'No news found.',
 	'RSS news feed'			=>	'RSS news feed'
