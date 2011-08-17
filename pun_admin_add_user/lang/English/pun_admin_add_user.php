13b26448d7b410a9b4bf75836d3398f70b9c2a64
dimkalinux <dimka.linux@gmail.com>
2011-08-17 21:37:14 +0300

    pun_admin_add_user: add to repo and update to PunBB 1.4

---
+++ pun_admin_add_user/lang/English/pun_admin_add_user.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Lang file for pun_admin_add_user
+ *
+ * @copyright Copyright (C) 2008-2011 PunBB
+ * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * @package pun_admin_add_user
+ */
+
+if (!defined('FORUM')) die();
+
+$lang_admin_add_user = array(
+	'User added successfully'					=> 'User has been added successfully.',
+	'Between 2 and 25 characters'				=> 'Between 2 and 25 characters.',
+	'Enter a current and valid e-mail address'	=> 'Enter a current and valid email address.',
+	'Edit User Identity after adding User'		=> 'Edit User Identity after adding User.',
+	'Add user'									=> 'Add user',
+	'There are some errors'						=> '<strong>Warning!</strong> There are some errors:',
+	'Username'									=> 'Username',
+	'E-mail'									=> 'Email',
+	'Edit user identity'						=> 'Edit User Identity'
+)
+
+?>
