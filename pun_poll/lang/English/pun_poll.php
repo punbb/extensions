f705f199ef901c14cd78ac7aaf1ecdde36701938
ashcs <vjacheslav.melnikov@gmail.com>
2012-03-28 11:10:56 +0700

    Marked as compatible with PunBB1.4.2

--- pun_poll/lang/English/pun_poll.php
+++ pun_poll/lang/English/pun_poll.php
@@ -3,7 +3,7 @@
 /**
  * Lang file for voting
  *
- * @copyright Copyright (C) 2008 PunBB
+ * @copyright Copyright (C) 2008-2012 PunBB
  * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  * @package pun_poll
  */
